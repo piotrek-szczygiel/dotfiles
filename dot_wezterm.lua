bf3f0bf00d3aa187c91ae14dd145f03cc509ab9b
Piotr Szczygieł <szczygiel.p@gmail.com>
2024-02-15 16:31:27 +0100

    update

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
+
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Ayu Dark (Gogh)'
@@ -7,6 +9,10 @@ config.font_size = 15
 config.initial_cols = 120
 config.initial_rows = 30
 
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
+
+config.keys = {
+  { key = 'k', mods = 'CMD', action = act.ClearScrollback 'ScrollbackAndViewport' },
+}
 
 return config
