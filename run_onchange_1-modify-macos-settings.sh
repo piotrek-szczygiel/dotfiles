8d8c309b13ce514109fbd16fe57a39416357d98a
Piotr Szczygieł <szczygiel.p@gmail.com>
2024-02-15 16:58:57 +0100

    update

--- run_onchange_1-modify-macos-settings.sh
+++ run_onchange_1-modify-macos-settings.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 defaults write -g com.apple.keyboard.fnState -bool true
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
+defaults write com.apple.dock show-recents -bool false
