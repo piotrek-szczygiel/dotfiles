48f2d33a4f33be81426033dca71c09149a74302c
Piotr Szczygieł <szczygiel.p@gmail.com>
2024-02-16 12:08:55 +0100

    update

--- run_onchange_2-shell.sh
+++ run_onchange_2-shell.sh
@@ -20,4 +20,3 @@ if ! grep -q "$touchid" /etc/pam.d/sudo_local; then
 fi
 
 fish -c "tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Dark --show_time=No --classic_prompt_separators=Vertical --powerline_prompt_heads=Round --powerline_prompt_tails=Sharp --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Many icons' --transient=No" > /dev/null
-fish -i
