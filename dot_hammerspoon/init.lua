cea3142fd5e42e4d5c98136a356f0d3fe7dcae27
Piotr Szczygieł <szczygiel.p@gmail.com>
2024-02-16 11:22:27 +0100

    update

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -35,7 +35,40 @@ hs.hotkey.bind({"alt"}, "2", toggleApp("com.microsoft.VSCode"))
 hs.hotkey.bind({"alt"}, "3", toggleApp("com.jetbrains.intellij"))
 hs.hotkey.bind({"alt"}, "4", toggleApp("company.thebrowser.Browser"))
 
-hs.hotkey.bind({"alt"}, "0", function() hs.alert.show(hs.application.frontmostApplication():bundleID()) end)
+hs.hotkey.bind({"alt"}, "0", function()
+    hs.alert.show(hs.application.frontmostApplication():bundleID())
+end)
+
+hs.caffeinate.set("system", true, false)
+
+fiddlerState = 0
+fiddler = hs.timer.new(1, function()
+    local p = hs.mouse.absolutePosition()
+
+    if fiddlerState == 0 then
+        hs.mouse.absolutePosition({x=p.x+20, y=p.y})
+        fiddlerState = 1
+    elseif fiddlerState == 1 then
+        hs.mouse.absolutePosition({x=p.x, y=p.y+20})
+        fiddlerState = 2
+    elseif fiddlerState == 2 then
+        hs.mouse.absolutePosition({x=p.x-20, y=p.y})
+        fiddlerState = 3
+    elseif fiddlerState == 3 then
+        hs.mouse.absolutePosition({x=p.x, y=p.y-20})
+        fiddlerState = 0
+    end
+end)
+
+hs.hotkey.bind({"cmd"}, "f12", function()
+    if fiddler:running() then
+        fiddler:stop()
+        hs.alert.show("Stopped")
+    else
+        fiddler:start()
+        hs.alert.show("Started")
+    end
+end)
 
 eventtap = hs.eventtap.new({hs.eventtap.event.types.systemDefined}, function(event)
     local systemKey = event:systemKey()
