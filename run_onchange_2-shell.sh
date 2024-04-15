830548046a604abf53441ecb24c2df59d7ea7b98
Piotr Szczygieł <szczygiel.p@gmail.com>
2024-04-15 18:21:42 +0200

    update

--- run_onchange_2-shell.sh
+++ run_onchange_2-shell.sh
@@ -7,11 +7,18 @@ fish="$(which fish)"
 if ! grep -q "$fish" /etc/shells; then
     echo "$fish" | sudo tee -a /etc/shells
 fi
-chsh -s "$fish"
+
+current="$(dscl . -read ~/ UserShell | sed 's/UserShell: //')"
+if [ "$current" != "$fish" ]; then
+    chsh -s "$fish"
+fi
 
 touchid="auth sufficient pam_tid.so"
 if ! grep -q "$touchid" /etc/pam.d/sudo_local; then
     echo "$touchid" | sudo tee -a /etc/pam.d/sudo_local
 fi
 
-git clone https://github.com/piotrek-szczygiel/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+kickstart="${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+if [ ! -d "$kickstart" ]; then
+    git clone https://github.com/piotrek-szczygiel/kickstart.nvim.git "$kickstart"
+fi
