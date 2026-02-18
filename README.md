8f2946d1b72e04e5ec73231c205df4b42c9b7e89
Piotr Szczygieł <szczygiel.p@gmail.com>
2026-02-18 11:37:39 +0100

    clean slate for yadm migration

--- README.md
+++
@@ -1,5 +0,0 @@
-# Installation
-
-```shell
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply piotrek-szczygiel
-```
--- run_onchange_1-settings.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-defaults write -g com.apple.keyboard.fnState -bool true
-defaults write -g InitialKeyRepeat -int 15
-defaults write -g KeyRepeat -int 2
-defaults write com.apple.dock show-recents -bool false
-defaults write com.apple.dock autohide-delay -int 0
-defaults write com.apple.dock autohide-time-modifier -float 0.4
