06dcfb9375f5ce4afedab8c5cbf533b7b8798fa8
Piotr Szczygieł <szczygiel.p@gmail.com>
2026-02-18 12:16:11 +0100

    initial yadm setup

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+set -gx EDITOR "zed --wait"
+set -gx VISUAL "zed --wait"
+
+alias q exit
+
+alias l "eza  --git --icons --group-directories-first"
+alias ls "eza --git --icons --group-directories-first"
+alias ll "eza --git --icons --group-directories-first -lF"
+alias la "eza --git --icons --group-directories-first -laF"
+
+alias ga "git add"
+alias gb "git branch --sort=committerdate | tac | grep -v '^\*' | fzf --height=20% | xargs git switch"
+alias gc "git commit"
+alias gco "git checkout --"
+alias gd "git diff"
+alias gl "git pull"
+alias glg "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gp "git push"
+alias gs "git status"
+
+alias tf "terraform"
+alias python "python3"
+
+set fish_greeting
+zoxide init fish | source
